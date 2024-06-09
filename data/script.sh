a657499c02457582a665308521f852a321388e1d
shreyash <1604011shreyash@gmail.com>
2024-06-09 16:45:36 +0100

    events with prices

--- data/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Combine all the files
-cat ./economic_events_* >> temp.csv
-
-# Remove the header lines from the files
-grep -v '^time' temp.csv >> economic_events.csv
-
-rm temp.csv
