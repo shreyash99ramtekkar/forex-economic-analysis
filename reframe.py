1fd45ae922a0359c0720d5419075f831644cba16
Shreyash <shreyash99ramtekkar>
2024-06-27 13:21:58 +0100

    CONSTANTS and 1,3,5h impact of the event

--- reframe.py
+++ reframe.py
@@ -3,10 +3,10 @@ import numpy as np
 from datetime import datetime ,timedelta
 
 
-CURRENCY = 'GBPUSD'
-HOURS_BEFORE=3
-HOURS_AFTER=24
-CURRENCY_INDEXES= ['-'+str(i)+'h' for i in range(1,HOURS_BEFORE)][::-1].extend([ str(i)+'h' for i in range(0,HOURS_AFTER+1)])
+CURRENCY = 'EURUSD'
+HOURS_BEFORE=4
+HOURS_AFTER=20
+# CURRENCY_INDEXES= ['-'+str(i)+'h' for i in range(0,HOURS_BEFORE)][::-1].extend([ str(i)+'h' for i in range(1,HOURS_AFTER+1)])
 
 # def attach_price(row):
 #     print(type(row.name))
@@ -21,14 +21,58 @@ currency_data.index = pd.to_datetime(currency_data.index)
 # economic_events.index = pd.to_datetime(economic_events.index)
 # economic_events.apply(attach_price,axis=1)
 
+
+
 def get_currency_prices_for_event(event_time, currency_prices, hours_before=5, hours_after=24):
-    start_time = event_time - timedelta(hours=hours_before)
+    # print('event time before' + event_time.strftime('%y-%m-%d %H:%M:%S'))
+    # flag=False
+    # if (event_time == datetime.strptime('2024-03-01 22:30:00', "%Y-%m-%d %H:%M:%S")):
+    #     print('event time before' + event_time.strftime('%y-%m-%d %H:%M:%S'))
+    #     flag=True
+    event_time = event_time.replace(minute=00)
+    start_time = event_time - timedelta(hours=hours_before-1)
     end_time = event_time + timedelta(hours=hours_after)
-    print(event_time)
-    start_to_event = currency_prices[start_time:event_time].close.values.tolist()
-    event_to_end = currency_prices[event_time:end_time].close.values.tolist()
-    print(start_to_event)
-    print(event_to_end)
+    start_to_event = currency_prices[start_time:event_time].close.values.flatten()
+    if len(start_to_event) < hours_before:
+        start_to_event = add_missing_times(currency_prices[start_time:event_time],event_time,hours_before).close.values.flatten()
+        # print(start_to_event)
+        # start_to_event = np.concatenate([([None]*(hours_before-len(start_to_event))),start_to_event])
+        # latest_value = pd.date_range(end=event_time, periods=hours_before, freq='h')[0]
+        # if (start_to_event is not None) and (latest_value == currency_prices[start_time:event_time].index[-1]):
+        #     print('asalamvalikum')
+        #     start_to_event = np.concatenate([start_to_event,([None]*(hours_before-len(start_to_event)))])
+        # else:
+        #     print('valikum salam')
+        #     start_to_event = np.concatenate([([None]*(hours_before-len(start_to_event))),start_to_event])
+        # if flag:
+        #     print('start to event '+ str(len(start_to_event)))
+        #     print('printing something')
+        #     print(start_to_event)
+        #     print(currency_prices[start_time:event_time])
+        #     flag=True
+    event_to_end = currency_prices[event_time:end_time].close.values.flatten()[1:]
+    if len(event_to_end) < hours_after:
+        event_to_end = add_missing_times(currency_prices[event_time:end_time],event_time,hours_after,True).close.values.flatten()[1:]
+        # event_to_end = np.concatenate([event_to_end,([None]*(hours_after-len(event_to_end)))])
+        # if flag:
+        #     print('event to end')
+        #     print(currency_prices[event_time:end_time][1:])
+        #     flag=True
+
+    start_to_end = np.concatenate([start_to_event,event_to_end],axis=0)
+    # if flag:
+    #     print('start to event '+ str(len(start_to_event)))
+    #     print(currency_prices[start_time:event_time])
+    #     print('event time after' +  event_time.strftime('%y-%m-%d %H:%M:%S'))
+    #     print('start to event '+ str(len(start_to_event)))
+    #     print(currency_prices[start_time:event_time])
+    #     print('event to end')
+    #     print(currency_prices[event_time:end_time][1:])
+    #     print('whole')
+    #     print(start_to_end)
+    #     print(pd.Series(start_to_end,index=[str(index)+'h' for index in range(-hours_before+1,hours_after+1)]))
+    return pd.Series(start_to_end,index=[str(index)+'h' for index in range(-hours_before+1,hours_after+1)])
+
     # if start_to_event is None :
     #     start_to_event = [None] * hours_before
     #     # print(start_to_event)
@@ -47,7 +91,28 @@ def get_currency_prices_for_event(event_time, currency_prices, hours_before=5, h
     # else:
     #     final_data.append(event_to_end)
    
-    return start_to_event.extend(event_to_end)
+
+def add_missing_times(df, event_time, hours,start=False):
+    # Generate the expected time intervals
+    if start:
+        expected_times = pd.date_range(start=event_time, periods=hours+1, freq='h')    
+    else:
+        expected_times = pd.date_range(end=event_time+timedelta(hours=1), periods=hours+1, freq='h')[:-1]
+    
+   
+    # Create a DataFrame with the expected times
+    expected_df = pd.DataFrame(index=expected_times)
+    
+    
+    # Merge the original DataFrame with the expected times DataFrame
+    combined_df = expected_df.join(df)
+    # if start:
+    #     print('combined dataframe')
+    #     print(df)
+    #     print('expected times dataframe')
+    #     print(expected_df)
+    #     print(combined_df.head(10))
+    return combined_df
 
 
 def attach_currency_data(row):
@@ -55,7 +120,7 @@ def attach_currency_data(row):
     # print(row)
     event_time = datetime.strptime(row.name, "%Y-%m-%d %H:%M:%S")
     prices = get_currency_prices_for_event(event_time, currency_data,hours_before=HOURS_BEFORE,hours_after=HOURS_AFTER)
-    return pd.Series(prices, index=CURRENCY_INDEXES)
+    return pd.Series(prices, index=prices.index)
 # Create a list to store results
 # Iterate through each economic event
 # for idx, event in economic_events.iterrows():
@@ -77,12 +142,15 @@ def attach_currency_data(row):
 
 print(economic_events_currency.head())
 columns = economic_events_currency.apply(attach_currency_data,axis=1)
+print(columns.shape)
 # Combine all results into a single DataFrame
-print(columns)
-final_df =  pd.concat([economic_events, columns], axis=1)
+print(economic_events_currency.shape)
+final_df =  pd.concat([economic_events_currency, columns], axis=1)
 
 # Display the combined DataFrame
-print(final_df)
+final_df.to_csv('./data/economic_events_new_'+ CURRENCY +'.csv')
+
+final_df.loc[final_df.index==datetime.strptime('2024-03-01 22:30:00', "%Y-%m-%d %H:%M:%S")]
 
 
 
